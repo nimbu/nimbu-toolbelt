7c7b5183f971f1947ec5128be79c1c8ae4254888
Peter Dedene <peter@zenjoy.be>
2022-01-14 14:22:40 +0100

    Add typescript interface output format

--- packages/cli/src/commands/channels/info.ts
+++ packages/cli/src/commands/channels/info.ts
@@ -3,9 +3,56 @@ import { flags } from '@oclif/command'
 import ux from 'cli-ux'
 import chalk from 'chalk'
 import { fetchAllChannels } from '../../utils/channels'
-import { isCalculatedField, isRelationalField, isSelectField } from '../../nimbu/types'
+import { CustomField, isCalculatedField, isRelationalField, isSelectField } from '../../nimbu/types'
+import { string } from '@oclif/parser/lib/flags'
 const Listr = require('listr')
 
+function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+const toPascalCase = (s: string) => {
+  return capitalizeFirstLetter(
+    s.replace(/([-_][a-z])/gi, ($1) => {
+      return $1.toUpperCase().replace('-', '').replace('_', '')
+    }),
+  )
+}
+
+const toInterfaceType = (field: CustomField) => {
+  switch (field.type) {
+    case 'string':
+    case 'text':
+    case 'email':
+    case 'calculated':
+      return 'string'
+    case 'integer':
+    case 'float':
+      return 'number'
+    case 'boolean':
+      return 'boolean'
+    case 'file':
+      return 'Nimbu.File'
+    case 'date':
+      return 'Nimbu.Date'
+    case 'time':
+    case 'date_time':
+      return 'Nimbu.DateTime'
+    case 'select':
+      return `Nimbu.Select<'${field.select_options.map((o) => o.name).join("' | '")}''>`
+    case 'multi_select':
+      return `Nimbu.MultiSelect<'${field.select_options.map((o) => o.name).join("' | '")}''>`
+    case 'belongs_to':
+    case 'customer':
+      return 'Nimbu.ReferenceTo'
+    case 'belongs_to_many':
+      return 'Nimbu.ReferenceMany'
+    case 'gallery':
+      return 'Nimbu.Gallery'
+    default:
+      return 'any'
+  }
+}
 export default class ChannelsInfo extends Command {
   static description = 'list info about this channel'
 
@@ -19,6 +66,11 @@ export default class ChannelsInfo extends Command {
 
   static flags = {
     ...ux.table.flags(),
+    output: flags.string({
+      exclusive: ['no-truncate', 'csv'],
+      description: 'output in a more machine friendly format',
+      options: ['csv', 'json', 'yaml', 'ts'],
+    }),
   }
 
   async execute() {
@@ -61,52 +113,61 @@ export default class ChannelsInfo extends Command {
       )
     }
 
-    ux.table(
-      channelForInfo.customizations,
-      {
-        label: {
-          get: (row) => (tty && row.required ? row.label + '*' : row.label),
-        },
-        name: {},
-        type: {},
-        reference: {
-          get: (row) => (isRelationalField(row) ? row.reference ?? '' : ''),
-        },
-        required: {
-          get: (row) => (tty ? (row.required ? '✓' : '') : !!row.required),
-        },
-        options: {
-          extended: true,
-          get: (row) => {
-            if (isSelectField(row)) {
-              return row.select_options.map((o) => `${o.name} (${o.slug} / ${o.id})\n`).join('')
-            } else {
-              return ''
-            }
+    if (flags.output === 'ts') {
+      this.log(`\nexport interface ${toPascalCase(channelForInfo.slug)} {`)
+      for (const field of channelForInfo.customizations) {
+        this.log(`  ${field.name}${field.required ? '' : '?'}: ${toInterfaceType(field)};`)
+      }
+      this.log('}')
+      return
+    } else {
+      ux.table(
+        channelForInfo.customizations,
+        {
+          label: {
+            get: (row) => (tty && row.required ? row.label + '*' : row.label),
+          },
+          name: {},
+          type: {},
+          reference: {
+            get: (row) => (isRelationalField(row) ? row.reference ?? '' : ''),
+          },
+          required: {
+            get: (row) => (tty ? (row.required ? '✓' : '') : !!row.required),
+          },
+          options: {
+            extended: true,
+            get: (row) => {
+              if (isSelectField(row)) {
+                return row.select_options.map((o) => `${o.name} (${o.slug} / ${o.id})\n`).join('')
+              } else {
+                return ''
+              }
+            },
+          },
+          hint: {
+            extended: true,
+            get: (row) => row.hint ?? '',
+          },
+          calculated_expression: {
+            extended: true,
+            get: (row) => (isCalculatedField(row) ? row.calculated_expression ?? '' : ''),
+          },
+          required_expression: {
+            extended: true,
+            get: (row) => row.required_expression ?? '',
+          },
+          unique: {
+            extended: true,
+            get: (row) => (tty ? (row.unique ? '✓' : '') : !!row.unique),
           },
         },
-        hint: {
-          extended: true,
-          get: (row) => row.hint ?? '',
+        {
+          printLine: this.log,
+          ...flags, // parsed flags
         },
-        calculated_expression: {
-          extended: true,
-          get: (row) => (isCalculatedField(row) ? row.calculated_expression ?? '' : ''),
-        },
-        required_expression: {
-          extended: true,
-          get: (row) => row.required_expression ?? '',
-        },
-        unique: {
-          extended: true,
-          get: (row) => (tty ? (row.unique ? '✓' : '') : !!row.unique),
-        },
-      },
-      {
-        printLine: this.log,
-        ...flags, // parsed flags
-      },
-    )
+      )
+    }
 
     if (tty) {
       const dependants = graph.dependantsOf(channel)
